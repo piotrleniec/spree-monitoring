5c862f42a4cfdde4ee2f2b0f3f4bcaacf09dab78
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-25 14:15:54 +0100

    wait until the database is ready to accept connections

--- shop/docker-entrypoint.sh
+++ shop/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+until [ "pg_isready -h database -U postgres" ]; do
+  sleep 0.5
+done
+
 if [ ! -f database_initialized ]; then
   rails db:migrate \
     && rails db:seed AUTO_ACCEPT=1 \
