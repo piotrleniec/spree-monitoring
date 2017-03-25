b9f748d5caf1a162bfb191eceac2e2dcb8e29906
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-25 12:32:03 +0100

    working database initialization

--- shop/docker-entrypoint.sh
+++ shop/docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 sleep 1
 
+if [ ! -f database_initialized ]; then
+  rails db:migrate \
+    && rails db:seed AUTO_ACCEPT=1 \
+    && rails spree_sample:load \
+    && touch database_initialized
+fi
+
 if [ ! -f assets_precompiled ]; then
   rails assets:precompile && touch assets_precompiled
 fi
