0ab52289c4f22c359f11568f4878cdae50c22221
Piotr Leniec <leniec.piotr@gmail.com>
2017-04-06 21:19:00 +0200

    little less buggy adding product to cart

--- traffic/index.js
+++ traffic/index.js
@@ -21,16 +21,21 @@ const addProductToCart = () =>
     .goto('http://localhost:3000')
     .evaluate(() => {
       const products = Array.from(document.querySelectorAll('.product-list-item'))
+      if (!products) return
+
       const product = products[Math.floor(Math.random() * products.length)]
+      if(! product) return
 
       return product.querySelector('a').href
     })
     .then(url => {
+      if (!url) return Promise.resolve()
+
       return nightmare
         .goto(url)
+        .wait('#quantity')
         .type('#quantity', 1 + Math.floor(5 * Math.random()))
         .click('#add-to-cart-button')
-        .wait()
         .then(() => Promise.resolve())
     })
 
@@ -41,7 +46,11 @@ addProductToCart()
 
     nightmare.end().then()
   })
-  .catch(error => { console.log(error) })
+  .catch(error => {
+    console.log(error)
+
+    nightmare.end().then()
+  })
 
 /*
 roam({ pageCount: 2 })
